2f637735a709223873c4b37b7261520cc2d974ba
quangloc.nguyen <you@example.com>
2020-11-12 10:20:45 +0700

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 
+//second
 @NgModule({
   declarations: [
     AppComponent
